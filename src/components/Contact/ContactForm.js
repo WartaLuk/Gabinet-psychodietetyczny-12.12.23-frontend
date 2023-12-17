42990468bc75ce011d8faeee27304749d8f3ddd3
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2023-12-17 16:06:25 +0100

    Update ContactForm.js

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const ContactForm = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [responseMessage, setResponseMessage] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      .post("/api/contact", { name, email, message })
-      .then((response) => {
-        // Obsługa po pomyślnym wysłaniu formularza
-      })
-      .catch((error) => {
-        console.error("Błąd podczas wysyłania formularza:", error);
-      });
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('/api/contact', { name, email, message })
+            .then(response => {
+                setResponseMessage('Mail został prawidłowo wysłany, a jego kopia została przesłana do właściciela.');
+            })
+            .catch(error => {
+                console.error('Błąd podczas wysyłania formularza:', error);
+                setResponseMessage('Wystąpił błąd podczas wysyłania wiadomości.');
+            });
+    };
 
   return (
     <div>
