8ac7321080f11099b95448734374903cd036fd1f
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2023-12-17 16:08:35 +0100

    Add Footer.js

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} Gabinet Psychodietetyczny Agaty JW
+      </p>
+      <p>Wszelkie prawa zastrzeżone</p>
+      <p>Kontakt: kontakt@gabinet-agaty-jw.pl</p>
+    </footer>
+  );
+};
+
+export default Footer;
